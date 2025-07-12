b0f2f1429b7867e6d8b0933c2b137f6e67cdb85a
Aditya Hansda <40536327+adityahansda@users.noreply.github.com>
2025-07-12 18:43:27 +0530

    Update appwrite.ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Databases, Account } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
+    .setEndpoint('https://nyc.cloud.appwrite.io/v1') // Your Appwrite Endpoint
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string); // Your project ID
 
 export const account = new Account(client);
